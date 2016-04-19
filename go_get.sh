afa5fa03f078bfdd02a8d2fc7ac4519325ff8f08
Purandar <purandarrao@gmail.com>
2016-04-19 12:07:20 -0700

    Update go_get.sh

--- go_get.sh
+++ go_get.sh
@@ -3,4 +3,4 @@ go get -u github.com/dlintw/goconf
 go get -u github.com/golang/glog
 go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
 go get -u github.com/miekg/dns
-go get -u githut.com/google/seesaw
+go get -u github.com/google/seesaw
